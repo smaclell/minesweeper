c966dc9cbefdca291a3acfc4e859db6335c16481
Scott MacLellan <s.j.maclell@gmail.com>
2024-04-04 23:32:08 -0400

    Improving the styling

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -18,8 +18,10 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="flex flex-col items-center justify-center">
+      <div className="z-10 max-w-5xl w-full flex flex-col items-center justify-center font-mono text-sm space-y-5">
+        <h1 className="text-lg">Minesweeper</h1>
+        <p>How hard would you like it to be?</p>
+        <div className="flex flex-col items-start justify-center">
           <button onClick={() => create(8, 8, 5)}>
             Easy 😀
           </button>
--- ui/src/app/world/page.tsx
+++ ui/src/app/world/page.tsx
@@ -105,7 +105,7 @@ export default function WorldPage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center flex flex-col justify-center font-mono text-sm">
         {world? <WorldView world={world} /> : <LoadingView />}
       </div>
     </main>
